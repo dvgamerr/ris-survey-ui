b2e58121d7b9d86e4f9329cda095e1e2f44c378e
Kananek Thongkam <info.dvgamer@gmail.com>
2019-07-10 10:19:52 +0700

    fixed line room.

--- server/line/index.js
+++ server/line/index.js
@@ -1,7 +1,7 @@
 const logger = require('@debuger')('LINE')
 const request = require('request-promise')
 // Import and Set Nuxt.js options
-const LINE_API = process.env.LINE_API || 'Ca2338af8e1ae465a2541acde69cd4e0c'
+const LINE_API = process.env.LINE_API || 'Cbdf461dd7136e1e7b570d0cb6a2ef9f5'
 module.exports = async (body, bot = 'survey', userId) => {
   await request({ method: 'PUT', url: `http://10.101.147.48:3000/${bot}/${userId || LINE_API}`, body, json: true })
   let text = body.type === 'flex' ? body.altText : ''
