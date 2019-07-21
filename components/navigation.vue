e69da8f99c40687dfaa64e75bf737b31579824d1
ChSorayuth <adamsoryut@gmail.com>
2019-07-21 18:37:20 +0700

    freetime edited to update

--- components/navigation.vue
+++ components/navigation.vue
@@ -1,15 +1,14 @@
 <template>
-  <b-navbar type="dark" variant="dark">
-    <!-- v-if="$route.name !== 'sign-in'" -->
+  <b-navbar v-if="$route.name !== 'sign-in'" type="dark" variant="dark">
     <b-navbar-brand to="/">
       <b>[POS]</b> SERVER SURVEY
     </b-navbar-brand>
     <b-navbar-nav class="ml-auto">
       <b-nav-item exact to="/">Home</b-nav-item>
       <b-nav-item to="/history">History</b-nav-item>
-      <!-- <b-nav-item-dropdown :text="$auth.user.name" right>
+      <b-nav-item-dropdown :text="$auth.user.name" right>
         <b-dropdown-item @click.prevent="onSignOut">Logout</b-dropdown-item>
-      </b-nav-item-dropdown> -->
+      </b-nav-item-dropdown>
     </b-navbar-nav>
   </b-navbar>
 </template>
@@ -17,10 +16,15 @@
 export default {
   created () {
   },
-  // methods: {
-  //   onSignOut () {
-  //     this.$auth.logout()
-  //   }
-  // }
+  methods: {
+    onSignOut () {
+      this.$auth.logout()
+    }
+  }
 }
 </script>
+
+
+<style>
+
+</style>
